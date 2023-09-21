ebb86a0e4191704085d92fb5d7284cb1de4efada
Ivan Tkachuk <116971836+seshotake@users.noreply.github.com>
2023-09-22 01:34:02 +0300

    Remove unnecessary import

--- BankDeposits.App/Models/Account.cs
+++ BankDeposits.App/Models/Account.cs
@@ -1,6 +1,5 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace BankDeposits.App.Models;
 
