f907734104e16411a0b78ed6ba347b8980f74f99
Ivan Tkachuk <116971836+seshotake@users.noreply.github.com>
2023-09-30 22:05:22 +0300

    Add script to initialize mongodb

--- initdb.d/mongo-init.js
+++ initdb.d/mongo-init.js
@@ -1,5 +1,5 @@
-'use BankDeposits'
-
+db = db.getSiblingDB('BankDeposits');
+db.createCollection('Bank');
 db.Bank.insertOne({
     "_id": ObjectId(),
     "LastName": "Doe",
@@ -20,37 +20,9 @@ db.Bank.insertOne({
             ]
         }
     ]
-})
-
-db.Bank.insertOne({
-    "_id": ObjectId(),
-    "LastName": "Doe",
-    "FirstName": "Jane",
-    "Patronymic": "Jasper",
-    "PassportSeries": "1234",
-    "PassportNumber": "567891",
-    "HomeAddress": "Kyiv, Ukraine, Home Street, 2",
-    "Accounts": [
-        {
-            "AccountNumber": "1234567891",
-            "Amount": NumberDecimal("200.00"),
-            "Deposits": [
-                {
-                    "Amount": NumberDecimal("100.00"),
-                    "Date": ISODate("2020-01-01T00:00:00Z"),
-                },
-                {
-                    "Amount": NumberDecimal("100.00"),
-                    "Date": ISODate("2020-02-01T00:00:00Z"),
-                },
-                
-            ]
-        }
-    ]
-})
-
+});
 for (let i = 1; i <= 10; i++) {
-    let depositorId = ObjectId(); // equivalent to NEWID()
+    let depositorId = ObjectId();
     let lastName = 'LastName' + i;
     let firstName = 'FirstName' + i;
     let patronymic = 'Patronymic' + i;
@@ -77,7 +49,7 @@ for (let i = 1; i <= 10; i++) {
         ]
     };
 
-    let randomDeposits = Math.floor(Math.random() * 3) + 1; // equivalent to CAST(RAND() * 3 AS INT) + 1
+    let randomDeposits = Math.floor(Math.random() * 3) + 1;
 
     for (let j = 1; j <= randomDeposits; j++) {
         let depositAmount = Math.random() * 1000;
